f82da0e1d4fd0dd8e8900de252b5b3a5463dab0b
GrandpaNewton <ivan.gromyko.2804@gmail.com>
2024-10-27 12:47:33 +0300

    Adding .gitignore and first test app

---
+++ ImageProcessor/ImageProcessor/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Drawing;
+using System.IO;
+
+namespace ImageProcessor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Socket _udpSocket;
+        Image _currentImage;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            _udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        private void SendMessage()
+        {
+            if(_currentImage == null)
+            {
+                return;
+            }
+
+            byte[] data = ImageToByteArray(_currentImage);
+
+            _udpSocket.Connect(new IPEndPoint(IPAddress.Parse(IpTextBox.Text), 8080));
+
+            _udpSocket.Send(data);
+            
+        }
+
+        private void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void ChooseFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".jpg";
+            fileDialog.Filter = "Jpeg Images (.jpg)|*.jpg";
+
+            var showDialogResult = fileDialog.ShowDialog();
+
+            if (showDialogResult.HasValue && showDialogResult.Value)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(fileDialog.FileName);
+                _currentImage = Image.FromFile(fileDialog.FileName);
+            }
+        }
+
+        private byte[] ImageToByteArray(Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, imageIn.RawFormat);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ ImageProcessor/ImageProcessorServerWPF/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Drawing.Imaging;
+
+namespace ImageProcessorServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int Port = 8080;
+        UdpClient _udpServer;
+        private TcpListener _listener;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetMessage()
+        {
+            while (true)
+            {
+                var result = await _listener.AcceptTcpClientAsync();
+
+                byte[] buffer = new byte[result.ReceiveBufferSize];
+                var bytes = await result.GetStream().ReadAsync(buffer, 0, buffer.Length);
+
+                using (var ms = new MemoryStream(buffer))
+                {
+                    var image = Image.FromStream(ms);
+                    image.Save(ms, ImageFormat.Png);
+                    ms.Position = 0;
+
+                    var bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.StreamSource = ms;
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.EndInit();
+
+                    ShowImage(bitmapImage);
+                }
+
+                InfoLabel.Content = $"Получено {bytes} байт\n\r" +
+                    $"Удаленный адрес: {result.Client.RemoteEndPoint}\r\n";
+            }
+        }
+
+        private void ShowImage(BitmapImage image)
+        {
+            sendingImage.Source = image;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _listener = new TcpListener(IPAddress.Any, Port);
+            _listener.Start();
+            
+            /*_udpServer = new UdpClient(new IPEndPoint(IPAddress.Any, Port));
+*/
+            var ipAddress = GetLocalIPAddress();
+            InfoLabel.Content = $"Сервер {ipAddress}:{Port} запущен.";
+
+            
+            
+            GetMessage();
+        }
+
+        private string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Локальный IP-адрес не найден.");
+        }
+    }
+}
